0a998a0d053d13fd15f46276f9e7aad241219f29
Kevin Smits <smitskecbs@gmail.com>
2026-05-30 22:23:06 +0200

    allow token builder cors for pinata api

--- api/upload-to-pinata.js
+++ api/upload-to-pinata.js
@@ -1,4 +1,28 @@
+const ALLOWED_ORIGIN =
+  "https://token-builder.cbs-coin.com";
+
+function setCorsHeaders(res) {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    ALLOWED_ORIGIN
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "POST, OPTIONS"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization"
+  );
+}
+
 export default async function handler(req, res) {
+  setCorsHeaders(res);
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Method not allowed" });
   }
